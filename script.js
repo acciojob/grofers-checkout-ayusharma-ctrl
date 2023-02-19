e182eaf558be026f04d402d9fe90c43aeee5c951
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-02-19 13:19:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+  let totalPrice = 0;
+  prices.forEach((e) => {
+    const value = parseInt(e.textContent);
+    if (!Number.isNaN(value)) {
+      totalPrice += value;
+    }
+  });
+  const totalPriceRow = document.createElement("tr");
+  totalPriceRow.id = "ans";
+  const totalPriceData = document.createElement("td");
+  const totalPriceAns = document.createElement("td");
+  totalPriceRow.appendChild(totalPriceData);
+  totalPriceRow.appendChild(totalPriceAns);
+  totalPriceData.textContent = "Total Price:"
+  totalPriceAns.textContent = `${totalPrice}`
+  const table = document.querySelector("table");
+  table.appendChild(totalPriceRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
